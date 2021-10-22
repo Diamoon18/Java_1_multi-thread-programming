24135117eb5a7a1271785f2089a38a440ebb25d1
Dianamoon18 <72127610+Diamoon18@users.noreply.github.com>
2021-10-23 01:13:41 +0200

    Update M_trapezow.java

--- PD_11_10/M_trapezow.java
+++ PD_11_10/M_trapezow.java
@@ -5,18 +5,23 @@ public class M_trapezow extends Thread{
 	private float dx;
 	
 	private double w_calka;
-	private int i;
+	private int i, n;
 
-	public M_trapezow(double w_calka, int i, float a, float dx) {
+	public M_trapezow(double w_calka, int i, float a, float dx, int n) {
 		this.a = a;
 		this.dx = dx;
 		this.w_calka = w_calka;
 		this.i = i;
+		this.n = n;
 		start();
 	}
 	
 	public void run() {
-		w_calka = (mojPrzyklad.funkcja1(a+i*dx)+mojPrzyklad.funkcja1(a+(i+1)*dx))*dx/2;
+		if(i == 0 || i == n) {
+			w_calka = mojPrzyklad.exampleTen(a+i*dx)/2;
+		} else {
+			w_calka = mojPrzyklad.exampleTen(a+i*dx);
+		}
 		System.out.println("M_trapezow nr-" + i + " ma wartosc calki = " + w_calka);
 		main_task_1.wynik_trapezy += w_calka;
 	}
